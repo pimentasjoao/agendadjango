477c96866dea788770037e6f350c3effc75799b2
Joao Antonio <joao.almeida@ifmg.edu.br>
2024-08-05 18:46:45 -0300

    Trabalhando com models e admin

--- project/settings.py
+++ project/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,10 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'    #pasta padrao de arquivos staticos quando serv desenvolvimento
 STATICFILES_DIRS = [
-    BASE_DIR / 'base_static',
+    BASE_DIR / 'base_static',    # pasta ou localização que o usuario definiu como sendo o local de arquivos estaticos no ambiente de desenvolvimento
 ]
+STATIC_ROOT = BASE_DIR / 'static'   #COLLECTSTATIC pasta de arquivos estaticos quando o servidor estiver em produção
+MEDIA_URL = 'media/'   #local de arquivos enviados pelo usuario em desenvolvimento
+MEDIA_ROOT = BASE_DIR / 'media/'   #local de arquivos enviados pelo usuario em producao
 
 # Default primary key field type
 # https://docs.djangoproject.com/\en/5.0/ref/settings/#default-auto-field
