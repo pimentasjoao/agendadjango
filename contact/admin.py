5a8f5423144988bb308670851cabcdf7915a1abf
Joao Antonio <joao.almeida@ifmg.edu.br>
2024-08-06 12:20:45 -0300

    Trabalhando com views

--- contact/admin.py
+++ contact/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact import models
 
 # Register your models here.
 
-@admin.register(Contact)
+@admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
     ordering = '-id',
@@ -13,3 +13,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_max_show_all = 200
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # id (primary key - automático)
@@ -11,6 +12,13 @@ from django.db import models
 # picture (imagem)
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -20,6 +28,8 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True,upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
