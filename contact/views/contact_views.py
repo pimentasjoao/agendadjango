eddf757700f5715e0b61232c1736bd0d5c61255a
Joao Pimenta-PC <joaopimenta.federal@gmail.com>
2024-08-06 17:52:48 -0300

    Aprendendo QuerySets

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 
 def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    context = {
+        'contacts' : contacts
+    }
     return render(
         request,
-        'contact/index.html',
+        'contact/index.html', context,
     )
